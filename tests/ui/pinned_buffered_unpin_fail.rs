272a212e6a136f7a1cd3eaeb7d239a88be83861b
Loi Chyan <loichyan@foxmail.com>
2025-07-05 15:34:19 +0800

    tests: make UI tests independent of rust-src

--- tests/ui/pinned_buffered_unpin_fail.rs
+++ tests/ui/pinned_buffered_unpin_fail.rs
@@ -1,6 +1,16 @@
 use std::marker::PhantomPinned;
+use std::ops::Deref;
+use std::pin::Pin;
 
 use dynify::{from_closure, Buffered, Dynify};
+
+// Wraps Pin::into_inner to prevent rustc from reporting errors from rust-src.
+fn unpin<P: Deref>(ptr: Pin<P>) -> P
+where
+    P::Target: Unpin,
+{
+    std::pin::Pin::into_inner(ptr)
+}
 
 fn main() {
     let mut stack = [0u8; 16];
@@ -8,5 +18,5 @@ fn main() {
     let val: Buffered<PhantomPinned> = init.init(&mut stack);
 
     let pinned = std::pin::pin!(val);
-    let _ = std::pin::Pin::into_inner(pinned); // fails
+    let _ = unpin(pinned); // fails
 }
