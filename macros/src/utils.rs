7f8573f3442598e80070a8fdacc3339a220316ac
Loi Chyan <loichyan@foxmail.com>
2025-07-21 21:52:00 +0800

    feat(macros): support non-function items in traits

--- macros/src/utils.rs
+++ macros/src/utils.rs
@@ -1,5 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::ToTokens;
+use syn::Attribute;
 
 macro_rules! as_variant {
     ($val:expr, $variant:path $(,)?) => {
@@ -20,24 +21,6 @@ impl<F: Fn(&mut TokenStream)> ToTokens for QuoteWith<F> {
     }
 }
 
-pub(crate) fn quote_outer(attrs: &[syn::Attribute]) -> impl '_ + ToTokens {
-    quote_with(move |tokens| {
-        attrs
-            .iter()
-            .filter(|a| matches!(a.style, syn::AttrStyle::Outer))
-            .for_each(|a| a.to_tokens(tokens))
-    })
-}
-
-pub(crate) fn quote_inner(attrs: &[syn::Attribute]) -> impl '_ + ToTokens {
-    quote_with(move |tokens| {
-        attrs
-            .iter()
-            .filter(|a| matches!(a.style, syn::AttrStyle::Inner(_)))
-            .for_each(|a| a.to_tokens(tokens))
-    })
-}
-
 /// Determines whether the supplied path matches an item in `std`.
 pub(crate) fn is_std(path: &syn::Path, mod1: &str, ty: &str) -> bool {
     path.is_ident(ty)
@@ -52,4 +35,25 @@ pub(crate) fn extract_inner_type(arg: &syn::PathArguments) -> Option<&syn::Type>
     let arg = as_variant!(arg, syn::PathArguments::AngleBracketed)?;
     let first = arg.args.first()?;
     as_variant!(first, syn::GenericArgument::Type)
+}
+
+/// Splits attributes into `#[outer]` and `#![inner]`.
+pub(crate) trait FilterAttrs<'a> {
+    type Filtered: Iterator<Item = &'a Attribute>;
+
+    fn outer(self) -> Self::Filtered;
+    fn inner(self) -> Self::Filtered;
+}
+impl<'a> FilterAttrs<'a> for &'a [Attribute] {
+    type Filtered = std::iter::Filter<std::slice::Iter<'a, Attribute>, fn(&&Attribute) -> bool>;
+
+    fn outer(self) -> Self::Filtered {
+        self.iter()
+            .filter(|attr| matches!(attr.style, syn::AttrStyle::Outer))
+    }
+
+    fn inner(self) -> Self::Filtered {
+        self.iter()
+            .filter(|attr| matches!(attr.style, syn::AttrStyle::Inner(_)))
+    }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,11 @@
 #[dynify_macros::dynify]
 #[allow(async_fn_in_trait)]
 pub trait MyAsync<'x, T> {
+    type A<'a, B: 'a, C: 'a>
+    where
+        Self: 'a;
+    const HHH: usize;
+
     async fn foo(&self, s: &str, g: (&str, &str)) -> usize;
     async fn foo2<'a, 'g0, A>(&'a self, s: &str, g: (&'g0 str, A, &str)) -> usize;
     async fn foo3<'a, 'g0, A>(this: &'a Self, s: &str, g: (&'g0 str, A, &str)) -> usize;
