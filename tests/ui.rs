7ccb685a31dc4d9935f44a0323edf46054235d45
Loi Chyan <loichyan@foxmail.com>
2025-08-08 19:56:16 +0800

    test: enable UI tests on nightly toolchains

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,10 +1,14 @@
-#[cfg_attr(any(miri, coverage), ignore)] // compile tests are meaningless for coverage
+#[cfg_attr(miri, ignore)] // compile tests are meaningless for Miri
 #[test]
 fn compile_tests() {
     let t = trybuild::TestCases::new();
-    if rustversion::cfg!(stable(1.80)) {
-        t.compile_fail("tests/compile_fail/*.rs"); //  pinned to avoid UI breakages
-    }
     t.pass("tests/compile_pass/*.rs");
     t.pass("macros/src/dynify_tests/*.rs");
+
+    if rustversion::cfg!(stable(1.80)) {
+        t.compile_fail("tests/compile_fail/*.rs");
+    } else {
+        // Skip UI tests that depend on rustc
+        t.compile_fail("tests/compile_fail/dynify_*.rs");
+    }
 }
